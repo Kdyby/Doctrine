60a428eb2b0045b24d7a86a0f296faa91ec15b62
Filip Procházka <hosiplan@gmail.com>
2013-01-17 20:35:46 +0100

    copyright years

--- src/Kdyby/Doctrine/exceptions.php
+++ src/Kdyby/Doctrine/exceptions.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the Kdyby (http://www.kdyby.org)
  *
- * Copyright (c) 2008, 2012 Filip Procházka (filip@prochazka.su)
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
  *
  * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
  */
