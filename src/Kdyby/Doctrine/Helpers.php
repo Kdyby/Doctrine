b1841709ff72057202472d43b1de90a0f5920fae
Filip Procházka <filip@prochazka.su>
2013-05-11 17:17:38 +0200

    Added helper loadFromFile() by @JanTvrdik, @vrana and @Majkl578

--- src/Kdyby/Doctrine/Helpers.php
+++ src/Kdyby/Doctrine/Helpers.php
@@ -76,4 +76,54 @@ class Helpers extends Nette\Object
 		return array_merge($output, $one ? : array(), $two ? : array());
 	}
 
+
+
+	/**
+	 * Import taken from Adminer, slightly modified
+	 * This implementation is aware of delimiters used for trigger definitions
+	 *
+	 * @author   Jakub Vrána, Jan Tvrdík, Michael Moravec
+	 * @license  Apache License
+	 */
+	public static function loadFromFile(Connection $connection, $file)
+	{
+		$db = $connection->getWrappedConnection();
+
+		$query = file_get_contents($file);
+
+		$delimiter = ';';
+		$offset = 0;
+		while ($query != '') {
+			if (!$offset && preg_match('~^\\s*DELIMITER\\s+(.+)~i', $query, $match)) {
+				$delimiter = $match[1];
+				$query = substr($query, strlen($match[0]));
+			} else {
+				preg_match('(' . preg_quote($delimiter) . '|[\'`"]|/\\*|-- |#|$)', $query, $match, PREG_OFFSET_CAPTURE, $offset); // should always match
+				$found = $match[0][0];
+				$offset = $match[0][1] + strlen($found);
+
+				if (!$found && rtrim($query) === '') {
+					break;
+				}
+
+				if (!$found || $found == $delimiter) { // end of a query
+					$q = substr($query, 0, $match[0][1]);
+
+					$db->query($q);
+
+					$query = substr($query, $offset);
+					$offset = 0;
+				} else { // find matching quote or comment end
+					while (preg_match('~' . ($found == '/*' ? '\\*/' : (preg_match('~-- |#~', $found) ? "\n" : "$found|\\\\.")) . '|$~s', $query, $match, PREG_OFFSET_CAPTURE, $offset)) { //! respect sql_mode NO_BACKSLASH_ESCAPES
+						$s = $match[0][0];
+						$offset = $match[0][1] + strlen($s);
+						if ($s[0] !== '\\') {
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+
 }
