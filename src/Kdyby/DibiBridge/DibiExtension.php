6009b83589e81f6bf80d352f6d6f5c946013f8cd
Filip Procházka <filip@prochazka.su>
2013-05-26 16:02:14 +0200

    Fixed compatilibility with dev nette after nette/nette@ba801a3d6c05ceaa26e4f282988808445530dc37

--- src/Kdyby/DibiBridge/DibiExtension.php
+++ src/Kdyby/DibiBridge/DibiExtension.php
@@ -15,6 +15,13 @@ use Nette;
 
 
 
+if (!class_exists('Nette\DI\CompilerExtension')) {
+	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
+	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
+	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
+	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
+}
+
 /**
  * @author Filip Procházka <filip@prochazka.su>
  */
