968471d53a0f05185d81a9cd3865f054b72669f9
Filip Procházka <filip@prochazka.su>
2013-06-30 15:00:07 +0200

    More robust fix for Nette compatibility

--- src/Kdyby/DibiBridge/DibiExtension.php
+++ src/Kdyby/DibiBridge/DibiExtension.php
@@ -17,9 +17,12 @@ use Nette;
 
 if (!class_exists('Nette\DI\CompilerExtension')) {
 	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
-	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
 	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
 	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
+}
+
+if (!class_exists('Nette\Configurator')) {
+	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
 }
 
 /**
