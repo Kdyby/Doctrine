7f007ae09c2fd4f914ba896d7da7387ec421c0e7
Filip Procházka <filip@prochazka.su>
2013-09-27 14:23:45 +0200

    Added ObjectMixin to classes inherited from doctrine

--- src/Kdyby/Doctrine/QueryBuilder.php
+++ src/Kdyby/Doctrine/QueryBuilder.php
@@ -30,4 +30,136 @@ class QueryBuilder extends Doctrine\ORM\QueryBuilder implements \IteratorAggrega
 		return $this->getQuery()->iterate();
 	}
 
+
+
+	/*************************** Nette\Object ***************************/
+
+
+
+	/**
+	 * Access to reflection.
+	 * @return \Nette\Reflection\ClassType
+	 */
+	public static function getReflection()
+	{
+		return new Nette\Reflection\ClassType(get_called_class());
+	}
+
+
+
+	/**
+	 * Call to undefined method.
+	 *
+	 * @param string $name
+	 * @param array $args
+	 *
+	 * @throws \Nette\MemberAccessException
+	 * @return mixed
+	 */
+	public function __call($name, $args)
+	{
+		return Nette\ObjectMixin::call($this, $name, $args);
+	}
+
+
+
+	/**
+	 * Call to undefined static method.
+	 *
+	 * @param string $name
+	 * @param array $args
+	 *
+	 * @throws \Nette\MemberAccessException
+	 * @return mixed
+	 */
+	public static function __callStatic($name, $args)
+	{
+		return Nette\ObjectMixin::callStatic(get_called_class(), $name, $args);
+	}
+
+
+
+	/**
+	 * Adding method to class.
+	 *
+	 * @param $name
+	 * @param null $callback
+	 *
+	 * @throws \Nette\MemberAccessException
+	 * @return callable|null
+	 */
+	public static function extensionMethod($name, $callback = NULL)
+	{
+		if (strpos($name, '::') === FALSE) {
+			$class = get_called_class();
+		} else {
+			list($class, $name) = explode('::', $name);
+		}
+		if ($callback === NULL) {
+			return Nette\ObjectMixin::getExtensionMethod($class, $name);
+		} else {
+			Nette\ObjectMixin::setExtensionMethod($class, $name, $callback);
+		}
+	}
+
+
+
+	/**
+	 * Returns property value. Do not call directly.
+	 *
+	 * @param string $name
+	 *
+	 * @throws \Nette\MemberAccessException
+	 * @return mixed
+	 */
+	public function &__get($name)
+	{
+		return Nette\ObjectMixin::get($this, $name);
+	}
+
+
+
+	/**
+	 * Sets value of a property. Do not call directly.
+	 *
+	 * @param string $name
+	 * @param mixed $value
+	 *
+	 * @throws \Nette\MemberAccessException
+	 * @return void
+	 */
+	public function __set($name, $value)
+	{
+		Nette\ObjectMixin::set($this, $name, $value);
+	}
+
+
+
+	/**
+	 * Is property defined?
+	 *
+	 * @param string $name
+	 *
+	 * @return bool
+	 */
+	public function __isset($name)
+	{
+		return Nette\ObjectMixin::has($this, $name);
+	}
+
+
+
+	/**
+	 * Access to undeclared property.
+	 *
+	 * @param string $name
+	 *
+	 * @throws \Nette\MemberAccessException
+	 * @return void
+	 */
+	public function __unset($name)
+	{
+		Nette\ObjectMixin::remove($this, $name);
+	}
+
 }
