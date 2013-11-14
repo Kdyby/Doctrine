dabed13e5f64e2f2961c325d0abc28905d879440
Michael Moravec <mail@majkl578.cz>
2013-11-14 20:03:38 +0100

    Added ReadOnlyCollectionWrapper

--- src/Kdyby/Doctrine/exceptions.php
+++ src/Kdyby/Doctrine/exceptions.php
@@ -403,3 +403,19 @@ class BatchImportException extends \RuntimeException implements Exception
 {
 
 }
+
+
+
+/**
+ * @author Michael Moravec
+ */
+class ReadOnlyCollectionException extends NotSupportedException
+{
+	/**
+	 * @throws ReadOnlyCollectionException
+	 */
+	public static function invalidAccess($what)
+	{
+		return new static('Could not ' . $what . ' read-only collection, write/modify operations are forbidden.');
+	}
+}
