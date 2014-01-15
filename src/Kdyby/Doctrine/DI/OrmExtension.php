70ca9a5a9734b7a36c77630f876105d9261d923e
LM <ladislav@marek.su>
2014-01-15 14:53:50 +0100

    fix types overwriting

--- src/Kdyby/Doctrine/DI/OrmExtension.php
+++ src/Kdyby/Doctrine/DI/OrmExtension.php
@@ -254,7 +254,7 @@ class OrmExtension extends Nette\DI\CompilerExtension
 				$providedTypes = $extension->getDatabaseTypes();
 				Validators::assert($providedTypes, 'array');
 
-				if (!isset($config['types'])) {
+				if (!isset($defaults['types'])) {
 					$defaults['types'] = array();
 				}
 
