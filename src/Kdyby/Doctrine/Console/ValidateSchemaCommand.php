ca5b8267a1b403ac68f8a673bd3889b1a576ad23
Filip Procházka <filip@prochazka.su>
2014-04-07 04:00:24 +0200

    Hack to simulate newer doctrine validate schema command options

--- src/Kdyby/Doctrine/Console/ValidateSchemaCommand.php
+++ src/Kdyby/Doctrine/Console/ValidateSchemaCommand.php
@@ -14,6 +14,7 @@ use Doctrine;
 use Kdyby;
 use Nette;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
@@ -39,11 +40,55 @@ class ValidateSchemaCommand extends Doctrine\ORM\Tools\Console\Command\ValidateS
 
 
 
+	protected function configure()
+	{
+		parent::configure();
+
+		if (!$this->getDefinition()->hasOption('skip-mapping')) {
+			$this->addOption('skip-mapping', null, InputOption::VALUE_NONE, 'Skip the mapping validation check');
+		}
+
+		if (!$this->getDefinition()->hasOption('skip-sync')) {
+			$this->addOption('skip-sync', null, InputOption::VALUE_NONE, 'Skip checking if the mapping is in sync with the database');
+		}
+	}
+
+
+
 	protected function initialize(InputInterface $input, OutputInterface $output)
 	{
 		parent::initialize($input, $output);
 		$this->cacheStorage->clean(array(Nette\Caching\Cache::ALL => TRUE));
 		Nette\Diagnostics\Debugger::$productionMode = FALSE;
+	}
+
+
+
+	/**
+	 * Hack to simulate behaviour of newer doctrine for older 2.4.* versions users.
+	 * It does the check but doesn't return exit code for that check.
+	 *
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int|null
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$exit = parent::execute($input, $output);
+
+		if ($input->getOption('skip-sync')) {
+			if ($exit >= 2) {
+				$exit -= 2;
+			}
+		}
+
+		if ($input->getOption('skip-mapping')) {
+			if ($exit === 1 || $exit === 3) {
+				$exit -= 1;
+			}
+		}
+
+		return $exit;
 	}
 
 }
