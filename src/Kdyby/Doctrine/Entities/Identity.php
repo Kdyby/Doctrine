0685813788b2ee30e101d509dfebaee89c38388f
Martin Štekl <martin.stekl@gmail.com>
2014-05-28 01:14:10 +0200

    Created entity identity trait

---
+++ src/Kdyby/Doctrine/Entities/Identity.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008, 2012 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.md that was distributed with this source code.
+ */
+
+namespace Kdyby\Doctrine\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+
+/**
+ * @author Martin Štekl <martin.stekl@gmail.com>
+ *
+ * @property-read int $id
+ */
+trait Identity
+{
+
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue
+	 * @var integer
+	 */
+	private $id;
+
+
+
+	/**
+	 * @return integer
+	 */
+	final public function getId()
+	{
+		return $this->id;
+	}
+
+
+
+	public function __clone()
+	{
+		$this->id = NULL;
+	}
+
+}
