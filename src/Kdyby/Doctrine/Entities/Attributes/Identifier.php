99360a083a68ff488c7bce621b74b58bcf442759
Filip Procházka <filip@prochazka.su>
2014-06-02 14:06:02 +0200

    Renamed Entities\Identifier to Entities\Attributes\Identifier

--- src/Kdyby/Doctrine/Entities/Attributes/Identifier.php
+++ src/Kdyby/Doctrine/Entities/Attributes/Identifier.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the file license.md that was distributed with this source code.
  */
 
-namespace Kdyby\Doctrine\Entities;
+namespace Kdyby\Doctrine\Entities\Attributes;
 
 use Doctrine\ORM\Mapping as ORM;
 
