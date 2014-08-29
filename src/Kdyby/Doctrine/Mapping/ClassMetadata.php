c85a5469d179763db8b5b445e2a920fd78139782
Michael Moravec <github.com@majkl578.cz>
2014-08-29 21:37:03 +0200

    ClassMetadata: Switch to using doctrine/instantiator [closes #128]

--- src/Kdyby/Doctrine/Mapping/ClassMetadata.php
+++ src/Kdyby/Doctrine/Mapping/ClassMetadata.php
@@ -23,11 +23,9 @@ class ClassMetadata extends Doctrine\ORM\Mapping\ClassMetadata
 {
 
 	/**
-	 * The prototype from which new instances of the mapped class are created.
-	 *
-	 * @var object
+	 * @var Doctrine\Instantiator\InstantiatorInterface|NULL
 	 */
-	private $_prototype;
+	private $instantiator;
 
 
 
@@ -50,23 +48,11 @@ class ClassMetadata extends Doctrine\ORM\Mapping\ClassMetadata
 	 */
 	public function newInstance()
 	{
-		if ($this->_prototype === null) {
-			if (PHP_VERSION_ID === 50429 || PHP_VERSION_ID === 50513 || PHP_VERSION_ID === 50600) {
-				if ($this->getReflectionClass()->implementsInterface('Serializable')) {
-					$this->_prototype = @unserialize(sprintf('C:%d:"%s":0:{}', strlen($this->name), $this->name));
-				}
-			}
-
-			if (!$this->_prototype) {
-				$this->_prototype = @unserialize(sprintf('O:%d:"%s":0:{}', strlen($this->name), $this->name));
-			}
-
-			if (!$this->_prototype) {
-				throw new Kdyby\Doctrine\UnexpectedValueException("Prototype of class {$this->name} cannot be created, probably due to some PHP bug.");
-			}
+		if ($this->instantiator === NULL) {
+			$this->instantiator = new Doctrine\Instantiator\Instantiator();
 		}
 
-		return clone $this->_prototype;
+		return $this->instantiator->instantiate($this->name);
 	}
 
 }
