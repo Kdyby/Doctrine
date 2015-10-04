381011675fa621af49c753de228ec835e0f348a5
Pavel Kouřil <pk@pavelkouril.cz>
2015-10-04 18:25:42 +0200

    added extension for doctrine migrations

---
+++ src/Kdyby/Doctrine/Console/StatusCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Doctrine\Console;
+
+use Doctrine;
+use Kdyby;
+use Nette;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+
+/**
+ * @author Pavel Kouřil <pk@pavelkouril.cz>
+ */
+class StatusCommand extends Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand
+{
+
+	/**
+	 * @var \Kdyby\Doctrine\Tools\CacheCleaner
+	 * @inject
+	 */
+	public $cacheCleaner;
+
+
+
+	protected function initialize(InputInterface $input, OutputInterface $output)
+	{
+		parent::initialize($input, $output);
+		$this->cacheCleaner->invalidate();
+	}
+
+}
---
+++ src/Kdyby/Doctrine/DI/MigrationsExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Doctrine\DI;
+
+use Doctrine;
+use Kdyby;
+use Nette;
+
+
+
+/**
+ * @author Pavel Kouřil <pk@pavelkouril.cz>
+ */
+class MigrationsExtension extends Nette\DI\CompilerExtension
+{
+	
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+		$services = $this->loadFromFile(__DIR__ . '/migrations.neon');
+
+		foreach ($services as $i => $command) {
+			$builder->addDefinition($this->prefix('cli.migrations.' . $i))
+				->addTag(Kdyby\Console\DI\ConsoleExtension::TAG_COMMAND)
+				->setInject(FALSE)
+				->setClass($command);
+		}
+	}
+	
+}
