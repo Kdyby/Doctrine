830a6514a7b408fe4aaff42cf0ff4ecd8fd21c91
Filip Procházka <filip@prochazka.su>
2015-11-13 16:49:26 +0100

    QO refactoring: remove pointless reflection

--- src/Kdyby/Doctrine/QueryObject.php
+++ src/Kdyby/Doctrine/QueryObject.php
@@ -228,7 +228,7 @@ abstract class QueryObject extends Nette\Object implements Kdyby\Persistence\Que
 
 		if (!$query instanceof Doctrine\ORM\AbstractQuery) {
 			throw new UnexpectedValueException(
-				"Method " . $this->getReflection()->getMethod('doCreateQuery') . " must return " .
+				"Method " . get_called_class() . "::doCreateQuery must return " .
 				"instanceof Doctrine\\ORM\\Query or Kdyby\\Doctrine\\QueryBuilder or Kdyby\\Doctrine\\DqlSelection, " .
 				(is_object($query) ? 'instance of ' . get_class($query) : gettype($query)) . " given."
 			);
