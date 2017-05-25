a3cce5e27eb0181b6856ff8932a69f471e441515
Filip Procházka <filip@prochazka.su>
2017-05-25 22:49:32 +0200

    phpdoc

--- src/Kdyby/Doctrine/Registry.php
+++ src/Kdyby/Doctrine/Registry.php
@@ -35,8 +35,13 @@ class Registry extends AbstractManagerRegistry
 	 */
 	private $serviceLocator;
 
-
-
+	/**
+	 * @param array $connections
+	 * @param array $managers
+	 * @param string $defaultConnection
+	 * @param string $defaultManager
+	 * @param \Nette\DI\Container $serviceLocator
+	 */
 	public function __construct(array $connections, array $managers, $defaultConnection, $defaultManager, Nette\DI\Container $serviceLocator)
 	{
 		parent::__construct('ORM', $connections, $managers, $defaultConnection, $defaultManager, 'Doctrine\ORM\Proxy\Proxy');
